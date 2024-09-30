4316b5c2f89ac8732e3487e0fb118861007a3c0b
zibby-zibs <oloyedetimi@gmail.com>
2024-09-30 01:05:04 +0100

    chore: Update npm dependencies to include @tanstack/react-query
    This commit updates the `package.json` and `package-lock.json` files to include the `@tanstack/react-query` library as a new dependency. The version added is `5.56.2`.

--- src/app/admin/_components/sidebar.tsx
+++ src/app/admin/_components/sidebar.tsx
@@ -60,7 +60,10 @@ const Sidebar = (props: Props) => {
       <div className="hidden border-r bg-muted/40 md:block h-svh">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-            <Link href="/" className="flex items-center gap-2 font-semibold">
+            <Link
+              href="/admin"
+              className="flex items-center gap-2 font-semibold"
+            >
               {/* <Pregnan className="h-6 w-6" /> */}
               <span className="">Admin</span>
             </Link>
